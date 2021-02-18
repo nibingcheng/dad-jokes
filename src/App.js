93c529c02666fb7bb34b75404674dca8a76f14f1
Bingcheng Ni <nibingcheng@johndeere.com>
2021-02-18 16:22:23 -0600

    completed

--- src/App.js
+++ src/App.js
@@ -1,9 +1,31 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
+import axios from "axios";
 import './App.css';
 
 class App extends Component {
+  constructor() {
+    super()
+
+    this.state = {
+      joke: ""
+    }
+  }
+  componentDidMount = () => {
+    axios.get('https://icanhazdadjoke.com/', {
+    headers: {
+        Accept: 'application/json'
+    }
+  }).then(response => {
+    console.log('response:', response.data.joke);
+      this.setState ({
+        joke: response.data.joke
+      });      
+    })
+  }
+
   render() {
+    console.log("Render this:", this.state.joke);
     return (
       <div className="App">
         <header className="App-header">
@@ -11,8 +33,9 @@ class App extends Component {
           <h1 className="App-title">Welcome to React</h1>
         </header>
         <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+           {this.state.joke} 
         </p>
+        <button onClick={this.componentDidMount}>New Joke</button>
       </div>
     );
   }
